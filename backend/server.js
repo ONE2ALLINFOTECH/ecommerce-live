7d1752737afca1783f05cbd038a1b43a0a8a4e3f
ONE2ALLINFOTECH <one2allinfotech@gmail.com>
2025-11-13 15:20:29 +0530

    updated some code

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,28 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+  origin: [
+    "https://ecomerce-indol-eight.vercel.app",
+    "http://localhost:3000"
+  ],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/auth-customer', require('./routes/authCustomerRoutes')); // Add this line
+app.use('/api/auth-customer', require('./routes/authCustomerRoutes'));
 app.use('/api/categories', require('./routes/categoryRoutes'));
 app.use('/api/brands', require('./routes/brandRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/cart', require('./routes/cart'));
 app.use('/api/orders', require('./routes/orderRoutes'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
